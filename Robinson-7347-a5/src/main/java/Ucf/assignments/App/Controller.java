3d4704163a0d3636407be10217b5f70f223b74ce
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 23:45:49 -0400

    Create Controller.java

---
+++ Robinson-7347-a5/src/main/java/Ucf/assignments/App/Controller.java
@@ -0,0 +1,13 @@
+package main.java.Ucf.assignments.App;
+
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.*;
+
+public class Controller {
+    @FXML private Button submitButton;
+    @FXML private TextField nameField;
+    @FXML private TextField serialField;
+    @FXML private TextField priceField;
+}
