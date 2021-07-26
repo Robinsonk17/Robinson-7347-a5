8cb9c010354bc09fa6675fb669987ecdc3f190ca
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 23:50:30 -0400

    Update ItemModel.java

--- Robinson-7347-a5/src/main/java/Ucf/assignments/App/ItemModel.java
+++ Robinson-7347-a5/src/main/java/Ucf/assignments/App/ItemModel.java
@@ -1,5 +1,8 @@
 package main.java.Ucf.assignments.App;
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  Copyright 2021 Keondez Robinson
+ */
 
 import java.util.ArrayList;
 import java.util.List;
